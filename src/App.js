86465f7dbd45730d11663f64fd9c25825781e303
vitorexfernandes <83621837+vitorexfernandes@users.noreply.github.com>
2023-11-27 18:13:07 +0000

    Date Test 1

--- src/App.js
+++ src/App.js
@@ -1,13 +1,57 @@
 import Banner from './components/Banner';
 import GamesBody from './components/GamesBody';
 
+
+
+
+
 function App() {
+  //Variables used to fetch data from the API via Java Script
+  let currentRequests = null; 
+  let maxRequests = null; 
+
+  //Variable that will contain the JSON data
+  let games = [];
+
+  //Variables that search for HTML elements
+  const containerMainGames= document.getElementById("mainContainer");
+  const selectedDateElement = document.getElementById("selectedDate");
+
+  //Date Variables
+  const dataAtual = new Date();
+
+  //Select start date 
+  selectedDateElement.textContent = dataAtual.toLocaleDateString() + " Games";
+
+  document.addEventListener("DOMContentLoaded", function () {
+    const dateButton = document.getElementById("date_picker_button");
+    const liveButton = document.getElementById("live_picker_button");
+    
+    // LIVE BUTTON
+    liveButton.addEventListener("click", function () {
+        selectedDateElement.textContent = dataAtual.toLocaleDateString() + " Games";
+    });
+  
+    // DATE BUTTON
+    flatpickr(dateButton, { dateFormat: "d-m-Y", onClose: 
+            function (selectedDates) 
+            {
+                if (selectedDates.length > 0) 
+                {
+                    selectedDateElement.textContent = selectedDates[0].toLocaleDateString() + " Games";
+                }
+            }
+        }); 
+  });
+
   return (
     <div className="App">
       <Banner />
-      <GamesBody />
+      <GamesBody  selectedDate={selectedDateElement.textContent} />
     </div>
   );
 }
 
 export default App;
+
+
--- src/components/GamesBody/GamesBody.js
+++ src/components/GamesBody/GamesBody.js
@@ -1,10 +1,10 @@
 import LeagueCard from '../LeagueCard'; // Remova as chaves { } aqui
 import './GamesBody.css'
 
-export const GamesBody = () => {
+export const GamesBody = (props) => {
   return (
     <section className="main-container" id="mainContainer">
-            <p className="title-body" id="selectedDate"></p>
+            <p className="title-body" id="selectedDate">{props.selectedDate}</p>
             <LeagueCard></LeagueCard>
     </section>
   );
