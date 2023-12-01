5ae871f4448859397042254e3a517fc68b0a7254
vitorexfernandes <83621837+vitorexfernandes@users.noreply.github.com>
2023-12-01 21:51:01 +0000

    bug

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -7,7 +7,7 @@ export const Banner = (props) => {
     <section className="banner">
       <div className="banner-background"></div>
       <div className="banner-content">
-        <img className="banner-image" src="/images/banner.png" alt="banner Champions" />
+        <img className="banner-image" src="/images/Banner.png" alt="banner Champions" />
         <div className="banner-text">Live Football</div>
         <div className="banner-button-container">
         <button className="banner-button" id="live_picker_button"  onClick={props.toggleLivePicker}>
